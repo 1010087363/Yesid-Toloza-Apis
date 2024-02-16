c88351efab81bfb318bb81688b5a2e54534759c6
Yesid Stiven Toloza Ruiz <yesid.toloza@sistemasgyg.com.co>
2024-02-16 14:13:18 -0500

    apis con dos ejercicios .net core

---
+++ ejer3/Controllers/CalcularPromedioFinal.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ejer3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CalcularPromedioFinal : ControllerBase
+    {
+        [HttpGet("{parcial1}/{parcial2}/{parcial3}/{examenParcial}/{trabajoFinal}")]
+        public ActionResult<object> CalculateGrade(int parcial1, int parcial2, int parcial3, int examenParcial, int trabajoFinal)
+        {
+            if (parcial1 <= 0 || parcial2 <= 0 || parcial3 <= 0 || examenParcial <= 0 || trabajoFinal <= 0)
+            {
+                return BadRequest("Las calificaciones deben ser enteros y positivos mayores que cero");
+            }
+
+            double promedioParciales = (parcial1 + parcial2 + parcial3) / 3;
+            double promedioFinal = (promedioParciales * 0.55) + (examenParcial * 0.30) + (trabajoFinal * 0.15);
+
+            promedioFinal = Math.Round(promedioFinal, 3);
+
+            return Ok(promedioFinal);
+        }
+    }
+}
+
---
+++ ejer3/Controllers/CalcularSueldo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ejer3.Controllers
+{
+    public class Employee
+    {
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "El nombre solo puede contener letras.")]
+        public required string Name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Las horas de trabajo deben ser un número entero positivo.")]
+        public int HoursWorked { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El valor de pago por hora debe ser un número entero positivo.")]
+        public decimal HourlyRate { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CalcularSueldo : ControllerBase
+    {
+        [HttpGet("{name}/{hoursWorked}/{hourlyRate}")]
+        public ActionResult<object> CalculateSalary(string name, int hoursWorked, decimal hourlyRate)
+        {
+            if (string.IsNullOrEmpty(name) || hoursWorked <= 0 || hourlyRate <= 0)
+            {
+                return BadRequest("Los parámetros de entrada son inválidos.");
+            }
+
+            decimal salary = hoursWorked * hourlyRate;
+
+            return Ok(salary);
+        }
+    }
+}
